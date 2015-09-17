7de3bc9c2a919b2359fd8764aac2a31f0075869a
Amy Cory <amandacorazon1973@gmail.com>
2015-09-16 19:14:38 -0500

    Repaso de variables y datos

---
+++ variables.rb
@@ -0,0 +1,20 @@
+# Defini una variable con un dato de cadena de texto.
+variable = "primera variable"
+# Defini un avariable con un dato tipo float
+miVariable = 1.67
+# Defini una variable tipo booleano
+booleanoVar = true
+# Defini una variable con dato float
+total = 4
+#defini una variable con una suma de dos variables
+sumatoria = 3 + 2
+#defini una variable con una suma de dos variables
+miVAr = total + sumatoria
+=begin
+se definen variables y se dan varios 
+=end
+
+
+puts miVAr
+print booleanoVar
+print total
