484acac7f137c2e64f5967fef82abb2d197aa8dc
Amy Cory <amandacorazon1973@gmail.com>
2015-09-16 20:03:15 -0500

    usi de gets, chomp, print y concatenacion

---
+++ gets.rb
@@ -0,0 +1,10 @@
+# pido que me de su primer nombre
+print "por favor su primer nombre"
+# pido que me de su apellido
+nombre = gets.chomp
+print "ahora dame tu apellido"
+nombre2 = gets.chomp
+
+puts "Hola," + nombre + " " + nombre2 + "."
+
+
