52213edb6d7bad19f0fe01cfed7a6cbc32c56c54
Amy Cory <amandacorazon1973@gmail.com>
2015-09-16 20:01:28 -0500

    comapracion de valores y retorno de mensajes egun el resultado.

---
+++ bloques.rb
@@ -0,0 +1,28 @@
+# creo un bloque en donde mostrara un mensaje \
+# si dos variables son iguales
+if "andres" == "andres"
+	puts "andres es igual a andres"
+end
+
+# provocando un error
+if "andres" == "pepito"
+	puts "andres es igual a andres"
+end
+
+# rescatando el valor que comparamos
+if "andres"  == "julio"
+	puts "andres es igual es julio"
+else
+	puts "andres no es igual a julio"
+end
+
+# prueba
+# cosntruy el bloque que me lo valide (2+5)
+
+if 7 == 2+5
+	puts "el resultado es 7"
+end
+
+
+
+
