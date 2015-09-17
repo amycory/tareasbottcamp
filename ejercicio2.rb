eec240c0d053cef4c52a481952b44b671b615af9
Amy Cory <amandacorazon1973@gmail.com>
2015-09-16 21:43:38 -0500

    minprograma para pedir numero de celular y validar numero

---
+++ ejercicio2.rb
@@ -0,0 +1,17 @@
+# Este sript deberia pedir el numnero de celular
+# en caso tal de que sea este numero deberia decir que 
+# numero ya esta en uso. (3133575494)
+# si el numero es difernete envia un mensaje de exito en la
+# seleccion del numero.
+# utilicen puts, gets, chomp, prints y un bloque
+
+# pido numero de celular
+prints "por favor escriba su numero celular:"
+# asigno el valor a una variable
+numero = gets.chomp
+
+# paso el avlor que ingresaron aun numero entero
+if numero_definitivo= 
+	puts "error, ese numero ya existe"
+else
+	puts "felicidades, cargaste tu celular"
