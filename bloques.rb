38249ca8a62f09e94b2c964c5b981c764afb9abe
Amy Cory <amandacorazon1973@gmail.com>
2015-09-16 21:38:44 -0500

    bloques con comentarios y elsif

--- bloques.rb
+++ bloques.rb
@@ -1,15 +1,22 @@
-# creo un bloque en donde mostrara un mensaje \
-# si dos variables son iguales
+# creo un bloque en donde mostrara un mensaje si dos valores\
+# valores son iguales
+# ojo use dos signos igual
+# un signo igualo asignara un valor
+#dos signos igual comparara los valroes
+
+puts "ejecutando linea 7."
 if "andres" == "andres"
 	puts "andres es igual a andres"
 end
 
 # provocando un error
+puts "ejecutando la linea 13."
 if "andres" == "pepito"
 	puts "andres es igual a andres"
 end
 
 # rescatando el valor que comparamos
+puts "ejecutando la linea 19."
 if "andres"  == "julio"
 	puts "andres es igual es julio"
 else
@@ -17,12 +24,22 @@ else
 end
 
 # prueba
-# cosntruy el bloque que me lo valide (2+5)
-
+# cosntruye el bloque que me lo valide el resultado de una suma (2+5)
+puts "ejecutando la linea 28."
 if 7 == 2+5
 	puts "el resultado es 7"
 end
 
+#bloque con dos condiciones.
+puts "***********"
+puts "ejecutando la linea 34."
+if "hola"== "chao"
+	puts "hola es igual a chao."
+elsif "chao" == "chao"
+	puts "chao es igual a chao"
+else
+puts "no se cumple con ninguna condicion"
+end
 
 
 
