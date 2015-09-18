5c1ea3013b1430bb96c476330761499457b6c947
Amy Cory <amandacorazon1973@gmail.com>
2015-09-17 21:43:31 -0500

    repaso de operadores basicos

---
+++ ejercicio5.rb
@@ -0,0 +1,35 @@
+variable = "Amanda"
+varUno = "Corazon"
+
+# Realiza la comparacion de dos variables.
+puts variable == varUno
+
+# Si las variables NO son iguales deberías presentar un mensaje de alerta.
+puts variable != varUno
+if !variable == varUno
+	puts "son variables diferentes"
+end
+
+# Realiza la comparacion de cuatro valores, cada uno de ellos deberia cumplirse.
+puts 8 > 7
+
+puts 25 >= 25
+
+if "amanda" == "amanda" || "hola"  == "hola"
+	puts "los nombre son iguales"
+end
+
+if "andres" == "andres" and "corazon" == "corazon"
+	puts "los nombres son iguales"
+end
+
+# Realiza la comparacion de dos numeros enteros, si son iguales muestra un mensaje de alerta.
+if 4 == 4
+	puts "los nuemros son iguales"
+end
+
+# Suma dos numeros enteros que como resultado den 5 y luego verifica si es el cinco 
+total = (3 + 2)
+
+# es mayor o igual a 7.
+puts total >= 7
