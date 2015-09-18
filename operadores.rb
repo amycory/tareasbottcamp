775bdb8e6a716f2ad83d91fe2bc1cac50b9b202b
Amy Cory <amandacorazon1973@gmail.com>
2015-09-17 21:17:16 -0500

    uso de operadores

---
+++ operadores.rb
@@ -0,0 +1,33 @@
+# Le asigno valor a una variable
+nombre = "Amanda"
+
+# Comparo mi nombre con el de pepe.
+puts nombre == "pepe"
+
+# Comparo mi nombre con el de pepe, pero quiero saber si es diferente.
+puts nombre != "pepe"
+
+# Verifico que mi nombre no sea igual a pepe.
+if !nombre == "pepe"
+	puts "son nombres diferentes."
+end
+
+# Tres es mayor que 4?
+puts 3 > 4
+
+# Cinco es ma yor o igual que 5?
+puts 5 >= 5
+
+# tres es menor o igual que tres?
+puts 3 <= 3
+
+# Es uno o es el otro, pero de que es, es!
+if 3 == 5 || 4 <= 9
+	puts "cuatro es menor a nueve"
+end
+
+# Se tiene que cumplir ambos.
+if "a" == "a" && "h" == "h"
+	puts "Ambos son iguales"
+end
+
