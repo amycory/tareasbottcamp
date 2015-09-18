24e691a8b61c6f3d8b9601cbfd9ccb66bd61e8fc
Amy Cory <amandacorazon1973@gmail.com>
2015-09-17 19:45:40 -0500

    ejercicio para corregir errores

---
+++ ejercicio3.rb
@@ -0,0 +1,18 @@
+Nombre = "Amanda"
+nombre = "amanda"
+EDAD = 40
+cuidad = "Bogota"
+
+# Bogota es igual a bogota.
+# Debería ejecutar el puts
+if cuidad == "Bogota" then
+	puts "la ciudad es Bogota"
+end
+
+#el arreglo deberia tener 5 elementos
+	arreglo = ["Manazana","Limon", 3, 2.4, "Pera"]
+
+
+# Quiero imprimir el ultimo elemento del arreglo.
+puts arreglo[4]
+
